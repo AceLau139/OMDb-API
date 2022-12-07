4a875e07cf0967cfef819b4a73db9c667dc6f787
Ace <113405563+AceLau139@users.noreply.github.com>
2022-12-07 19:56:28 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 //http://www.omdbapi.com/?apikey=bf9d768b&s={keyword}
 //apikey=bf9d768b
-var Url = 'http://www.omdbapi.com/?apikey=bf9d768b'
-var searchBar = document.querySelector('#search-bar');
-var result = document.querySelector('.result');
+const Url = 'http://www.omdbapi.com/?apikey=bf9d768b'
+const searchBar = document.querySelector('#search-bar');
+const result = document.querySelector('.result');
+const moviesSection = document.querySelector('#movies-section');
 
 async function getData(url) {
   const res = await fetch(url);
   const data = await res.json();
   console.log(data);
+
+  result.innerHTML = '<h3 id="movies-section" class="sub-heading">Movies</h3>';
+
   if (data.Response == 'True') {
     console.log(data.Search);
-    const resultItem = document.createElement('li');
-    //resultItem.className = 'resultItem';
-    resultItem.innerHTML = '<li class="resultItem">'+ data.Search.Title +'</li>';
+
+    const resultItem = document.createElement('div'); 
+    resultItem.innerHTML = '<div class="resultItem">'+ data.Search[0].Title +'</div>';
+    resultItem.innerHTML += '<div class="resultItem">'+ data.Search[1].Title +'</div>';
+    resultItem.innerHTML += '<div class="resultItem">'+ data.Search[2].Title +'</div>';
+
     console.log(resultItem);
     result.insertAdjacentElement('beforeend', resultItem);
   }
